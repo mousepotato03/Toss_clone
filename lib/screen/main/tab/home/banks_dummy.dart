0b97e30a92b3da8c25cba606bd62fbe853da6fb9
seungjin-cloud <seungjin2@kau.kr>
2024-07-28 01:20:29 +0900

    홈화면

---
+++ lib/screen/main/tab/home/banks_dummy.dart
@@ -0,0 +1,6 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank.dart';
+
+final bankShinhan = Bank('신한은행', '$basePath/bank/bank_shinhan.png');
+final bankKakao = Bank('신한은행', '$basePath/bank/bank_kakao.png');
+final bankToss = Bank('신한은행', '$basePath/bank/bank_toss.png');
